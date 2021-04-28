86720d9aabc31dd688023f2e8b591120cb89e501
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-29 04:48:07 +0900

    adoc

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   const [idCounter, setIdCounter] = useState(0);
   const [todos, setTodo] = useState([]);
 
+  /**
+   * フォーム送信したらtodo配列にtodoを追加
+   * @param {Event} e 送信イベント
+   */
   const handleSubmit = (e) => {
     e.preventDefault();
     const inputText = e.target["task"].value;
@@ -12,8 +16,26 @@ function App() {
     setTodo([...todos, { id: nextid, task: inputText, checked: false }]);
   };
 
+  /**
+   * 指定idをtodo配列から取り除く
+   * @param {number} id
+   */
   const handleClickDeleteButton = (id) => {
     setTodo(todos.filter((todo) => todo.id !== id));
+  };
+
+  /**
+   * TODOのチェックボックスがクリックされたら該当の checked フラグを toggle する
+   * @param {number} id
+   */
+  const handleChangeCheckBox = (id) => {
+    const changedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, checked: !todo.checked };
+      }
+      return todo;
+    });
+    setTodo(changedTodos);
   };
 
   return (
@@ -24,8 +46,11 @@ function App() {
       </form>
       <div>
         {todos.map((todo) => (
-          <div key={todo.id}>
-            <input type="checkbox" />
+          <div key={todo.id} className={todo.checked ? "checked" : ""}>
+            <input
+              type="checkbox"
+              onChange={() => handleChangeCheckBox(todo.id)}
+            />
             {todo.task}
             <button onClick={() => handleClickDeleteButton(todo.id)}>
               削除
